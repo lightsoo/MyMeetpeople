0b57a4b53725f7b04c09557a0437ef6a42f6d86f
LG <kks3805@naver.com>
2016-03-07 16:51:23 +0900

    app_bar

--- app/src/main/java/com/meetpeople/lightsoo/mymeetpeople/CommonAdapter/TabAdapter.java
+++ app/src/main/java/com/meetpeople/lightsoo/mymeetpeople/CommonAdapter/TabAdapter.java
@@ -4,6 +4,11 @@ import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentStatePagerAdapter;
 
+import com.meetpeople.lightsoo.mymeetpeople.Fragment.ChatFragment;
+import com.meetpeople.lightsoo.mymeetpeople.Fragment.HomeFragment;
+import com.meetpeople.lightsoo.mymeetpeople.Fragment.MyinfoFragment;
+import com.meetpeople.lightsoo.mymeetpeople.Fragment.WriteFragment;
+
 /**
  * Created by LG on 2016-03-07.
  */
@@ -17,18 +22,27 @@ public class TabAdapter extends FragmentStatePagerAdapter{
         Fragment resultFragement = null;
         Class fragmentClass = null;
         switch (position){
-            case 0 :
+            case 0 : fragmentClass = HomeFragment.class;
                 break;
-            case 1 :
+            case 1 : fragmentClass = ChatFragment.class;
                 break;
-            case 2 :
+            case 2 : fragmentClass = MyinfoFragment.class;
                 break;
-            case 3 :
-                //여기서 프레그먼트 생성안하고 바고 인텐트호출이 될것도 같다...
+            //여기서 프레그먼트 생성안하고 바고 인텐트호출이 될것도 같다...
+            case 3 : fragmentClass = WriteFragment.class;
                 break;
             default:
-                fragmentClass = ()
+                //첫화면을 MainActivity에서 설정 할수있는데, 안된다면 여기서 설정!
         }
+
+        try {
+            resultFragement = (Fragment)fragmentClass.newInstance();
+        } catch (InstantiationException e) {
+            e.printStackTrace();
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        }
+
         return resultFragement;
     }
 
